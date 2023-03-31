90785178627eefc07852b19c56ec9027a88ed638
Crowdbotics <team@crowdbotics.com>
2023-03-31 16:10:43 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "tight-meadow-39632",
+  url: "https://tight-meadow-39632.botics.co",
+  api: "https://tight-meadow-39632.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
